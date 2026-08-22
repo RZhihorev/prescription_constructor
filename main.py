6483a8b9119b0372e04305697b2683e9bf027697
Roman Zhihorev <rzhihorev@yandex.ru>
2026-08-22 23:26:37 +0300

    feat: добавить генерацию рецепта 107-1/у из Word-шаблона
    Реализовано заполнение шаблона через docxtpl с подстановкой даты, времени, данных пациента, врача и лекарственного назначения.
    
    Добавлено автоматическое сохранение сформированного документа на Рабочем столе с отметкой даты и времени в имени файла.
    
    README дополнен инструкциями по установке зависимостей, структуре проекта, настройке Jinja-плейсхолдеров, запуску программы и устранению типичных ошибок.

---
+++ main.py
@@ -0,0 +1,34 @@
+from datetime import date, datetime
+from docxtpl import DocxTemplate
+from pathlib import Path
+
+
+today = date.today()
+d = today.day
+m = today.month
+y = today.strftime("%y")
+current_time = datetime.now().strftime('%H-%M-%S')
+
+patient_fio = 'Иванов И.И.'
+patient_birthdate = '01.01.1900'
+doctor_fio = 'Разрушовский Г.Т.'
+drug_1_form_name_dosage = 'Tab. Doxiciclini 0.1 №60'
+drug_1_signa = 'S.: Внутрь по 1 таблетке 2 раза в день 30 дней'
+
+BASE_DIR = Path(__file__).resolve().parent
+template_path = BASE_DIR / 'templates/prescription_107-1u_template.docx'
+desktop_path = Path.home() / 'Desktop'
+output_path = desktop_path / f'{patient_fio} {today} {current_time}.docx'
+
+doc = DocxTemplate(template_path)
+doc.render({
+    'd': d,
+    'm': m,
+    'y': y,
+    'patient_fio': patient_fio,
+    'patient_birthdate': patient_birthdate,
+    'doctor_fio': doctor_fio,
+    'drug_1_form_name_dosage': drug_1_form_name_dosage,
+    'drug_1_signa': drug_1_signa
+    })
+doc.save(output_path)
