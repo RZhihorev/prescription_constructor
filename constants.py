bb8334fdc58c4edb7751f915a1176163e3978a00
Roman Zhihorev <rzhihorev@yandex.ru>
2026-09-12 09:05:43 +0300

    style: some ui changes

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-APP_VERSION = '0.2.3'
-BUILD_DATE = '07.09.2026'
+APP_VERSION = '0.2.4'
+BUILD_DATE = '12.09.2026'
 COPYRIGHT = '© 2026 Жихорев Р.С. Конструктор рецептов 107-1/у'
--- main.py
+++ main.py
@@ -90,8 +90,8 @@ if __name__ == '__main__':
         url=HTML_PATH.as_uri(),
         js_api=PrescriptionAPI(),
         width=600,
-        height=640,
-        min_size=(600, 640),
+        height=650,
+        min_size=(600, 650),
     )
 
     webview.start(center_window, (window,))
