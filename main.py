6d62d140b429e54aa6cb0ac734790239266b7007
Roman Zhihorev <rzhihorev@yandex.ru>
2026-09-19 20:23:41 +0300

    feat: add info button, next will be info message

--- main.py
+++ main.py
@@ -103,8 +103,8 @@ if __name__ == '__main__':
         url=HTML_PATH.as_uri(),
         js_api=PrescriptionAPI(),
         width=600,
-        height=680,
-        min_size=(600, 680),
+        height=670,
+        min_size=(600, 670),
     )
 
     webview.start(center_window, (window,))
